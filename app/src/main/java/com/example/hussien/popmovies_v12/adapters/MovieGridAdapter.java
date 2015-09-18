73bf792bd484e352a87110d32569dd4e90a86c47
bold <hussien.fcih2016@gmail.com>
2015-09-18 17:02:30 +0200

    no message

--- app/src/main/java/com/example/hussien/popmovies_v12/adapters/MovieGridAdapter.java
+++ app/src/main/java/com/example/hussien/popmovies_v12/adapters/MovieGridAdapter.java
@@ -33,70 +33,6 @@ public class MovieGridAdapter extends  CursorAdapter {
         super(context,c,flags);
         mContext = context;
         mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//        mObjects = objects;
-    }
-
-    public Context getContext() {
-        return mContext;
-    }
-
-    public void add(Movie object) {
-        synchronized (mLock) {
-            mObjects.add(object);
-        }
-        notifyDataSetChanged();
-    }
-
-    public void clear() {
-        synchronized (mLock) {
-            mObjects.clear();
-        }
-        notifyDataSetChanged();
-    }
-
-    public void setData(List<Movie> data) {
-        clear();
-        for (Movie movie : data) {
-            add(movie);
-        }
-    }
-
-    @Override
-    public int getCount() {
-        return mObjects.size();
-    }
-
-    @Override
-    public Movie getItem(int position) {
-        return mObjects.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View view = convertView;
-        ViewHolder viewHolder;
-
-        if (view == null) {
-            view = mInflater.inflate(R.layout.grid_item_movie, parent, false);
-            viewHolder = new ViewHolder(view);
-            view.setTag(viewHolder);
-        }
-
-        final Movie movie = getItem(position);
-
-        String image_url = "http://image.tmdb.org/t/p/w185" + movie.getImage();
-
-        viewHolder = (ViewHolder) view.getTag();
-
-        Glide.with(getContext()).load(image_url).into(viewHolder.imageView);
-        viewHolder.titleView.setText(movie.getTitle());
-
-        return view;
     }
 
     @Override
@@ -107,19 +43,12 @@ public class MovieGridAdapter extends  CursorAdapter {
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        ViewHolder holder = new ViewHolder(view);
-        ImageView imageView = holder.imageView;
-        TextView titleView = holder.titleView;
-        int movieImageColumn = cursor.getColumnIndex(MovieContract.MovieEntry.COLUMN_IMAGE);
-        String moviePoster = cursor.getString(movieImageColumn);
-        Uri imageUri = Uri.parse("http://image.tmdb.org/t/p/").buildUpon()
-                .appendPath(context.getString(R.string.api_image_size_medium))
-                .appendPath(moviePoster.substring(1))
-                .build();
+String image_url = "http://image.tmdb.org/t/p/w185" + movie.getImage();
 
-        Glide.with(context).load(imageUri)
-                .placeholder(R.drawable.loading)
-                .into(imageView);
+        viewHolder = (ViewHolder) view.getTag();
+
+        Glide.with(getContext()).load(image_url).into(viewHolder.imageView);
+        viewHolder.titleView.setText(movie.getTitle());
     }
 
     public static class ViewHolder {
